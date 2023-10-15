dbcd7dfe7088f147a8ee6fdc73361cec9d7a6cd0
maximoortelli <maximoortelli@hotmail.com>
2023-10-14 22:44:18 -0300

    Update files
    Update files

---
+++ src/components/Doctors/DoctorCard.jsx
@@ -0,0 +1,34 @@
+import starIcon from '../../assets/images/Star.png'
+import { Link } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
+
+const DoctorCard = ({ doctor }) => {
+  const {
+    name,
+    avgRating,
+    totalRating,
+    photo,
+    specialization,
+    totalPatient,
+    hospital,
+  } = doctor;
+
+  return (
+    <div className="p-3 lg:p-4">
+      <div>
+        <img src={photo} className="w-full" alt="" />
+      </div>
+
+      <h2 className="text-[18px] leading-[30px] 
+      lg:text-[26px] lg:leading-9 text-headingColor 
+      font-[700] mt-3 lg:mt-5">{ name }</h2>
+
+      <div className="mt-2 lg:mt-4 flex items-center justify-between">
+        <span className='bg-[#CCF0F3] text-irisBlueColor 
+        py-1 px-2 lg:py-2 lg:px-6 text-[18px]'>{specialization}</span>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorCard;
