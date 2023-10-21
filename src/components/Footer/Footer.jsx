f307029de94168e269094290bc9c49433df63a0e
maximoortelli <maximoortelli@hotmail.com>
2023-10-21 01:07:53 -0300

    Update footer
    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -77,30 +77,30 @@ const Footer = () => {
 const year = new Date().getFullYear();
 
   return (
-  <footer className="pb-16 pt-10">
-     <div className="container">
-      <div className="flex justify-between 
-      flex-col md:flex-row flex-wrap gap-[30px]">
-        <div>
-          <img src={logo} alt="" />
-          <p className="text-[16px] leading-7 font-[400]
-          text-textColor mt-4">
-             Copyright {year} developed by Maximo Ortelli all right reserved.
-          </p>
+    <footer className="pb-[-100px] mb-[-50px]">
+  <div className="container">
+    <div className="flex flex-col md:flex-row flex-wrap gap-[30px] justify-center items-center">
+      <div className="flex flex-col items-center"> {/* Agregamos estas clases */}
+        <img src={logo} alt="" />
+        <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+          Copyright {year} developed by Maximo Ortelli all rights reserved.
+        </p>
 
-          <div className="flex items-center gap-3 mt-4">
-            {socialLinks.map((link, index) => (
-            <Link to={link.path} key={index} 
-            className="w-9 h-9 border-[#181A1E] 
-            rounded-full flex items-center justify-center
-            group hover:bg-primaryColor hover:border-none">
+        <div className="flex items-center gap-3 mt-4">
+          {socialLinks.map((link, index) => (
+            <Link
+              to={link.path}
+              key={index}
+              className="w-9 h-9 border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+            >
               {link.icon}
-            </Link>))}
-          </div>
+            </Link>
+          ))}
         </div>
       </div>
-     </div>
-  </footer>
+    </div>
+  </div>
+</footer>
 )};
 
 export default Footer;
