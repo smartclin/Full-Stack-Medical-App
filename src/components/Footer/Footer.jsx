20dcf1a6c8b44e2ee640fd2548d457809aee9308
maximoortelli <maximoortelli@hotmail.com>
2023-10-22 01:07:37 -0300

    Update all files
    Update all files

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,19 +8,15 @@ import { AiOutlineInstagram } from "react-icons/ai";
 const socialLinks = [
   {
     path: "https://www.youtube.com/watch?v=b3KeJ8DFjwQ",
-    icon: <AiFillYoutube className="group-hover:text-white w-4 h-5" />,
+    icon: <AiFillYoutube className="group-hover:text-white w-7 h-7" />,
   },
   {
     path: "https://www.youtube.com/watch?v=b3KeJ8DFjwQ",
-    icon: <AiFillGithub className="group-hover:text-white w-4 h-5" />,
+    icon: <AiOutlineInstagram className="group-hover:text-white w-7 h-7" />,
   },
   {
     path: "https://www.youtube.com/watch?v=b3KeJ8DFjwQ",
-    icon: <AiOutlineInstagram className="group-hover:text-white w-4 h-5" />,
-  },
-  {
-    path: "https://www.youtube.com/watch?v=b3KeJ8DFjwQ",
-    icon: <RiLinkedinFill className="group-hover:text-white w-4 h-5" />,
+    icon: <RiLinkedinFill className="group-hover:text-white w-7 h-7" />,
   },
 ];
 
@@ -79,26 +75,72 @@ const year = new Date().getFullYear();
   return (
     <footer className="pb-[-100px] mb-[-50px]">
   <div className="container">
-    <div className="flex flex-col md:flex-row flex-wrap gap-[30px] justify-center items-center">
+    
+    <div className="flex lg:justify-between sm:flex-col md:flex-row flex-wrap gap-[30px] msm:justify-center sm:items-center">
+      
       <div className="flex flex-col items-center"> {/* Agregamos estas clases */}
-        <img src={logo} alt="" />
-        <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+        
+        <img src={logo} alt="" className="lg:ml-[-18vw]"/>
+        
+        <p className="text-[16px] leading-7 font-[600] text-textColor lg:pl-[100px] mt-4 ">
           Copyright {year} developed by Maximo Ortelli all rights reserved.
         </p>
 
-        <div className="flex items-center gap-3 mt-4">
+        <div className="flex items-center gap-3 mt-4 lg:ml-[-20vw]">
           {socialLinks.map((link, index) => (
             <Link
               to={link.path}
               key={index}
-              className="w-9 h-9 border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+              className="border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none"
             >
               {link.icon}
             </Link>
           ))}
         </div>
+        
       </div>
+
+    <div className="lg:pl-[19%] max-sm:m-auto">
+      <h2 className="text-[20px] leading-[30px]  font-[700] mb-6 text-headingColor ">
+        Quick Links
+      </h2>
+
+      <ul>
+        {quickLinks01.map((item, index)=> ( 
+        <li key={index} className="mb-4 justify-center text-center"> 
+            <Link to={item.path}
+              className="text-[16px] leading-7
+              font-[400] text-textColor"
+            >
+              {item.display}
+            </Link>
+        </li> 
+        ))}
+      </ul>
     </div>
+
+    <div className="lg:pr-[5vw] max-sm:m-auto">
+      <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-center text-headingColor ">
+        Sections
+      </h2>
+
+      <ul>
+        {quickLinks02.map((item, index)=> ( 
+        <li key={index} className="mb-4 justify-center text-center"> 
+            <Link to={item.path}
+              className="text-[16px] leading-7
+              font-[400] text-textColor"
+            >
+              {item.display}
+            </Link>
+        </li> 
+        ))}
+      </ul>
+
+    </div>
+
+    </div>
+
   </div>
 </footer>
 )};
--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -44,7 +44,7 @@ const ServiceCard = ({ item, index }) => {
           to="/doctors"
           className="w-[44px] h-[44px] rounded-full
              border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center
-             justify-center group hover:bg-primaryColor hover:border-none"
+             justify-center group bg-red hover:border-none"
         >
           <BsArrowRight className="group-hover:text-white w-6 h-5 flex " />
         </Link>
