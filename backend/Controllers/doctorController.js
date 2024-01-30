9d45b2ac302e3789866b4744982de64f03b04173
maximoortelli <maximoortelli@hotmail.com>
2024-01-29 23:14:20 -0300

    Add all files
    Added all Routes.
    Added all Modules.
    Added all Controllers.
    Added authentication.
    Added verifyToken.js
    Installed node.js

---
+++ backend/Controllers/doctorController.js
@@ -0,0 +1,84 @@
+import Doctor from "../models/DoctorSchema.js";
+
+export const updateDoctor = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const updatedDoctor = await Doctor.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully updated",
+      data: updatedDoctor,
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Failed to update" });
+  }
+};
+
+export const deleteDoctor = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await Doctor.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully deleted",
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Failed to deleted" });
+  }
+};
+
+export const getSingleDoctor = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const doctor = await Doctor.findById(id).populate("reviews").select("-password");
+
+    res.status(200).json({
+      success: true,
+      message: "Doctor found",
+      data: doctor,
+    });
+  } catch (err) {
+    res.status(404).json({ success: false, message: "No Doctor found" });
+  }
+};
+
+export const getAllDoctor = async (req, res) => {
+  try {
+    const { query } = req.query;
+    let doctors;
+
+    //permite filtrar por el buscador de find a doctor y encontrar los doctores de la base de datos.
+    if (query) {
+      doctors = await Doctor.find({
+        isApproved: "approved",
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { specialization: { $regex: query, $options: "i" } },
+        ],
+      }).select("-password");
+    } else {
+      //Esta parte te ayuda para no relevar la informacion del password del usuario en el Api y que el sitio sea mas seguro
+      // Si sacamos la parte " isApproved: "approved" " Nos dara toda la informacion de los usuarios.
+      doctors = await Doctor.find({ isApproved: "approved" }).select(
+        "-password"
+      );
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Doctors found",
+      data: doctors,
+    });
+  } catch (err) {
+    res.status(404).json({ success: false, message: "Not found" });
+  }
+};
